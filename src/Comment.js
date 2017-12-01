493ee77b0f9c11fd83670bbb885d92fb5e49765c
MatteBru <mbruni616@gmail.com>
2017-12-01 11:33:44 -0500

    messy but doned

--- src/Comment.js
+++ src/Comment.js
@@ -1,3 +1,39 @@
 class Comment {
-  
+  constructor(img, path){
+    this.img = img;
+    this.path = path;
+    this.startUp();
+  }
+
+  startUp(){
+    let cmts = document.querySelector('#comments')
+    this.img.comments.forEach(c => {
+      this.make(c.content)
+    })
+    let cButton = document.querySelector('form').children[1]
+    cButton.addEventListener('click',(e) => { e.preventDefault();this.commenter();})
+  }
+
+  make(text){
+    let cmts = document.querySelector('#comments')
+    let cmt = document.createElement('li');
+    cmt.innerHTML = `<p>${text}</p>`
+    cmts.appendChild(cmt);
+  }
+
+
+  commenter(){
+    let newC = document.querySelector('#comment_input')
+    this.make(newC.value);
+
+
+    let options = {method: "POST",
+      headers: {'Accept': 'application/json','Content-Type': 'application/json'},
+      body: JSON.stringify({image_id: this.img.id, content: newC.value})
+    }
+    fetch(`${this.path}`, options)
+    newC.value = ''
+  }
+
+
 }
--- src/Image.js
+++ src/Image.js
@@ -1,4 +1,75 @@
 class Image {
-  
+  constructor(url, likeUrl, commentsURL){
+    this.url = url;
+    // this.likeUrl = likeUrl;
+    // const lurl = likeUrl
+    this.curl = `https://randopic.herokuapp.com/comments/`;
+    this.comments;
+    this.src;
+    this.id;
+    this.name;
+    this.likes;
+    this.fetchImg();
+    this.likeStart();
+  }
+
+
+  fetchImg(){
+    fetch(this.url).then(res => res.json()).then(json => this.saveSelf(json))
+  }
+
+  saveSelf(json){
+    this.name = json.name
+    this.likes = json.like_count
+    this.id = json.id
+    this.src = json.url
+    this.comments = json.comments;
+    this.addSelf()
+    const cmt = new Comment(this, this.curl)
+  }
+
+  addSelf(){
+    console.log(this);
+    let img = document.querySelector('#image');
+    let name = document.querySelector('#name');
+    let likes = document.querySelector('#likes');
+
+    img.setAttribute('src', this.src);
+    name.innerText = this.name;
+    likes.innerText = this.likes;
+  }
+
+  // likeStart(){
+  //   let likeButton = document.getElementById('like_button')
+  //   likeButton.addEventListener('click', this.liker)
+  // }
+
+  // likeStart(){
+  //   let likeButton = document.getElementById('like_button')
+  //   likeButton.addEventListener('click', function() {
+  //     let likes = document.querySelector('#likes')
+  //     likes.innerText = Number.parseInt(likes.innerText) + 1;
+  //     let options = {method: "POST",
+  //       headers: {'Accept': 'application/json','Content-Type': 'application/json'},
+  //       body: JSON.stringify({image_id: 19})
+  //     }
+  //     fetch(`https://randopic.herokuapp.com/likes/`, options).then(res => res.json()).then(json => console.log(json))
+  //   })
+  // }
+
+  likeStart(){
+    let likeButton = document.getElementById('like_button')
+    likeButton.addEventListener('click', this.liker.bind(this))
+  }
+
+  liker(){
+    let likes = document.querySelector('#likes')
+    likes.innerText = Number.parseInt(likes.innerText) + 1;
+    let options = {method: "POST",
+      headers: {'Accept': 'application/json','Content-Type': 'application/json'},
+      body: JSON.stringify({image_id: this.id})
+    }
+    fetch(`https://randopic.herokuapp.com/likes/`, options).then(res => res.json()).then(json => console.log(json))
+  }
 
 }
