62eb6f2de36dc1e13c35d4979d47ec818764d324
github username <github email address>
2017-12-01 11:04:03 -0500

    submitting

--- src/index.js
+++ src/index.js
@@ -1,7 +1,34 @@
 document.addEventListener('DOMContentLoaded', function() {
   const imageId = 1 //Enter your assigned imageId here
-  const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
+  const imageURL = `https://randopic.herokuapp.com/images/${43}`
   const likeURL = `https://randopic.herokuapp.com/likes/`
   const commentsURL = `https://randopic.herokuapp.com/comments/`
+  function getInfo() {
+    fetch(`${imageURL}`)
+    .then(res => res.json())
 
-})
+    .then(imageObj => bigFunc(imageObj.url))
+
+  }
+
+  function bigFunc(imageObj){
+
+    let imageContainer = document.getElementById("image")
+    imageContainer.setAttribute('src', `${imageObj}`);
+    let imageCard = document.getElementById("image_card")
+
+    imageCard.appendChild(imageContainer)
+      let newCom = document.getElementById('comment_form')
+      let input = newCom.value
+      let newLi = document.createElement('li')
+      newLi.innerHTML = input
+
+      let list = document.getElementById('comments')
+      list.appendChild(newLi)
+
+
+      }
+
+
+      getInfo()
+    })
